dd2a83fa2b2d7309d15be63a4845f7a8005dd1d7
Anagha Sethuraman <anagha.sethuraman@sjsu.edu>
2021-03-21 20:34:55 -0700

    Lab 3 : Confusion Matrix

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,126 +1,104 @@
+import pandas as pd
 import numpy as np
-import os
-from mlxtend.data import loadlocal_mnist
-import platform
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 
-class Detector:
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from tabulate import tabulate
 
-    def __init__(self):
-        np.random.seed(42)
-        # To plot pretty figures
-        mpl.rc('axes', labelsize=14)
-        mpl.rc('xtick', labelsize=12)
-        mpl.rc('ytick', labelsize=12)
-        # Where to save the figures
-        #PROJECT_ROOT_DIR = "."
-        #IMAGE_DIR = "FIXME"
-
-    # def save_fig(self,fig_id, tight_layout=True):
-    #     path = os.path.join(PROJECT_ROOT_DIR, "images", IMAGE_DIR, fig_id + ".png")
-    #     print("Saving figure", fig_id)
-    #     if tight_layout:
-    #         plt.tight_layout()
-    #     plt.savefig(path, format='png', dpi=300)
+        
+class DiabetesClassifier:
+    def __init__(self) -> None:
+        col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+        self.pima = pd.read_csv('diabetes.csv', header=0, names=col_names, usecols=col_names)
+        print(self.pima.head())
+        self.X_test = None
+        self.y_test = None
         
 
-    # def random_digit(self):
-    #     import matplotlib as mpl
-    #     import matplotlib.pyplot as plt 
-    #     some_digit = X[36000]
-    #     some_digit_image = some_digit.reshape(28, 28)
-    #     plt.imshow(some_digit_image, cmap = mpl.cm.binary,
-    #             interpolation="nearest")
-    #     plt.axis("off")
-
-    #     save_fig("some_digit_plot")
-    #     plt.show()
-
+    def define_feature(self, feature_cols):
+        X = self.pima[feature_cols]
+        y = self.pima.label
+        return X, y
     
-    def load_and_sort(self):
-        try:
-            from sklearn.datasets import fetch_openml
-            mnist = fetch_openml('mnist_784', version=1, cache=True)
-            #mnist.target = mnist.target.astype(np.int8) # fetch_openml() returns targets as strings
-            #self.sort_by_target(mnist) # fetch_openml() returns an unsorted dataset
-        except ImportError:
-            from sklearn.datasets import fetch_mldata
-            mnist = fetch_mldata('MNIST original')
-        return mnist["data"], mnist["target"]
-        
-
-    def sort_by_target(self,mnist):
-        reorder_train = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[:60000])]))[:, 1]
-        reorder_test = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[60000:])]))[:, 1]
-        mnist.data[:60000] = mnist.data[reorder_train]
-        mnist.target[:60000] = mnist.target[reorder_train]
-        mnist.data[60000:] = mnist.data[reorder_test + 60000]
-        mnist.target[60000:] = mnist.target[reorder_test + 60000]
+    def train(self,feature_cols,tol):
+        # split X and y into training and testing sets
+        X, y = self.define_feature(feature_cols)
+        X_train, self.X_test, y_train, self.y_test = train_test_split(X, y, random_state=0)
+        # train a logistic regression model on the training set
+        logreg = LogisticRegression(tol=tol)
+        logreg.fit(X_train, y_train)
+        return logreg
+    
+    def predict(self, feature_cols,tol):
+        model = self.train(feature_cols,tol)
+        y_pred_class = model.predict(self.X_test)
+        return y_pred_class
 
 
-    def train_predict(self,X,y):  
-        X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
-        shuffle_index = np.random.permutation(60000)
-        X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
-
-        # Example: Binary number 4 Classifier
-        # y_train_4 = (y_train == 4)
-        # y_test_4 = (y_test == 4)
-
-        # Number-5 detector binary classifier
-        y_train = y_train.astype(np.int8)
-        y_test = y_test.astype(np.int8)
-        y_train_5 = (y_train == 5)
-        y_test_5 = (y_test == 5)
-        
-        
-        from sklearn.linear_model import SGDClassifier
-        from sklearn.preprocessing import StandardScaler
-        from sklearn.pipeline import make_pipeline
-        clf = make_pipeline(StandardScaler(),
-           SGDClassifier(max_iter=1000, tol=1e-3))
-        clf.fit(X_train, y_train_5)
-
-        # cross validation
-        self.calculate_cross_val_score(clf,X_train,y_train_5)
-
-        # print prediction of the given input some_digit
-        some_digit = X[36000]
-        print(clf.predict([some_digit]))
-
-        # print prediction for the test dataset
-        y_pred = clf.predict(X_test)
-        # finding accuracy of the prediction
-        y_acc = (y_test_5 == y_pred)
-        y_acc_t = y_acc[y_acc == True]
-        print("Accuracy = " , len(y_acc_t)/len(y_acc))
+    def calculate_accuracy(self, result):
+        return metrics.accuracy_score(self.y_test, result)
 
 
-
-        
-    def calculate_cross_val_score(self,sgd_clf,X_train,y_train_5):
-        from sklearn.model_selection import cross_val_score
-        val=cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
-        print(val)
-
+    def examine(self):
+        dist = self.y_test.value_counts()
+        print(dist)
+        percent_of_ones = self.y_test.mean()
+        percent_of_zeros = 1 - self.y_test.mean()
+        return self.y_test.mean()
+    
+    def confusion_matrix(self, result):
+        return metrics.confusion_matrix(self.y_test, result)
+    
 if __name__ == "__main__":
-    obj=Detector()
-    # X, y = loadlocal_mnist(
-    #     images_path='train-images-idx3-ubyte', 
-    #     labels_path='train-labels-idx1-ubyte')
-    # #print('Dimensions: %s x %s' % (X.shape[0], X.shape[1]))
-    # #print('\n1st row', X[0])
-    # #print('Digits:  0 1 2 3 4 5 6 7 8 9')
-    # #print('labels: %s' % np.unique(y))
-    # #print('Class distribution: %s' % np.bincount(y))
-    # np.savetxt(fname='images.csv', 
-    #        X=X, delimiter=',', fmt='%d')
-    # np.savetxt(fname='labels.csv', 
-    #        X=y, delimiter=',', fmt='%d')
-    X,y=obj.load_and_sort()
-    #print(X)
-    #print(y)
-    X=X.values
+    classifer = DiabetesClassifier()
+    # Baseline
+    feature_cols = ['pregnant', 'insulin', 'bmi', 'age']
+
+    result = classifer.predict(feature_cols,0.0001)
+        #print(f"Predicition={result}")
+    score = classifer.calculate_accuracy(result)
+    print(f"score={score}")
+    con_matrix = classifer.confusion_matrix(result)
+    print(f"confusion_matrix=${con_matrix}")
+    list1=[['Baseline',score,con_matrix,'Baseline features']]
+    # Solution 1
+    feature_cols = ['pregnant','glucose', 'insulin', 'bmi', 'skin']
+    result = classifer.predict(feature_cols,0.0001)
+        #print(f"Predicition={result}")
+    score = classifer.calculate_accuracy(result)
+    print(f"score={score}")
+    con_matrix = classifer.confusion_matrix(result)
+    print(f"confusion_matrix=${con_matrix}")
+    list2=['Solution 1',score,con_matrix,'using features : pregnant,glucose,insulin,bmi,skin']
+    list1.append(list2)
+
+    # Solution 2
+    feature_cols = ['pregnant','glucose', 'insulin', 'bmi', 'age']
+    result = classifer.predict(feature_cols,0.0001)
+        #print(f"Predicition={result}")
+    score = classifer.calculate_accuracy(result)
+    print(f"score={score}")
+    con_matrix = classifer.confusion_matrix(result)
+    print(f"confusion_matrix=${con_matrix}")
+    list2=['Solution 2',score,con_matrix,'using features : pregnant,glucose,insulin,bmi,age']
+    list1.append(list2)
+
     
-    obj.train_predict(X,y)
+    # Solution 3
+    feature_cols = ['pregnant','glucose','insulin', 'bmi', 'age','bp','skin']        
+    result = classifer.predict(feature_cols,10)
+    #print(f"Predicition={result}")
+    score = classifer.calculate_accuracy(result)
+    print(f"score={score}")
+    con_matrix = classifer.confusion_matrix(result)
+    print(f"confusion_matrix=${con_matrix}")
+    list2=['Solution 3',score,con_matrix,'using features : pregnant,glucose,insulin,bmi,age,bp,skin and tol=10']
+    list1.append(list2)
+
+    df=np.array(list1)
+    print('___________________________________________________________')
+    print("Expected Output")
+    print('___________________________________________________________')
+    print(tabulate( df,headers=['Experiment', 'Accuracy', 'Confusion Matrix', 'Comments']))
+    print('___________________________________________________________')t
